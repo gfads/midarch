fd64d2ad9eea9532edb9d59d3627a3a77174b9b6
nsrosa <nsr@cin.ufpe.br>
2019-10-23 18:21:40 -0300

    New version - Development ok, Execution ok, MAPE-K complete, Versioning (replace operation) - Package reorganization

--- src/gmidarch/development/components/receiver.go
+++ src/gmidarch/development/components/receiver.go
@@ -2,13 +2,13 @@ package components
 
 import (
 	"fmt"
-	graphs2 "gmidarch/development/artefacts/graphs"
-	messages2 "gmidarch/development/messages"
+	"gmidarch/development/artefacts/graphs"
+	"gmidarch/development/messages"
 )
 
 type Receiver struct {
 	Behaviour string
-	Graph     graphs2.ExecGraph
+	Graph     graphs.ExecGraph
 }
 
 func NewReceiver() Receiver {
@@ -20,26 +20,6 @@ func NewReceiver() Receiver {
 	return *r
 }
 
-/*
-func (r *Receiver) Configure(invP *chan messages.SAMessage) {
-
-	// configure the state machine
-	msg := new(messages.SAMessage)
-	r.Graph = *graphs.NewExecGraph(2)
-	newEdgeInfo := graphs.ExecEdgeInfo{ExternalAction: element.Element{}.InvP, Message: msg, ActionChannel: invP, ActionType: 2}
-	r.Graph.AddEdge(0, 1, newEdgeInfo)
-
-	actionChannel := make(chan messages.SAMessage)
-	args := make([]*interface{}, 1)
-	args[0] = new(interface{})
-	*args[0] = msg
-	newEdgeInfo = graphs.ExecEdgeInfo{InternalAction: shared.Invoke, ActionName: "I_Printmessage", Message: msg, ActionType: 1, ActionChannel: &actionChannel, Args:args}
-	r.Graph.AddEdge(1, 0, newEdgeInfo)
-
-	return
-}
-*/
-
-func (Receiver) I_Printmessage(msg *messages2.SAMessage, info [] *interface{}) {
-	fmt.Printf("Receiver:: %v  \n",*msg)
+func (Receiver) I_Printmessage(msg *messages.SAMessage, info [] *interface{}) {
+	fmt.Printf("Receiver:: %v  \n", *msg)
 }
--- src/gmidarch/execution/ee/ee.go
+++ src/gmidarch/execution/ee/ee.go
@@ -1,14 +1,14 @@
 package ee
 
 import (
-	madl2 "gmidarch/development/artefacts/madl"
-	components2 "gmidarch/development/components"
+	"gmidarch/development/artefacts/madl"
+	"gmidarch/development/components"
 	"gmidarch/execution/engine"
-	shared2 "shared"
+	"shared"
 )
 
 type EE struct {
-	MADLX madl2.MADL
+	MADLX madl.MADL
 }
 
 func NewEE() EE {
@@ -25,23 +25,23 @@ func (e EE) Start() {
 		// Configure Unit's Info with Element's Info (Only components)
 		if e.MADLX.Components[i].TypeName == "Unit" { // TODO - Generalise for any component having 'Info'
 			tempElem := *e.MADLX.Components[i].Info[0]
-			unit := elem.(components2.Unit)
+			unit := elem.(components.Unit)
 			unit.UnitId = e.MADLX.Components[i].ElemId
-			unit.ElemOfUnit= tempElem.(madl2.Element).Type
-			unit.GraphOfElem = tempElem.(madl2.Element).Graph
+			unit.ElemOfUnit= tempElem.(madl.Element).Type
+			unit.GraphOfElem = tempElem.(madl.Element).Graph
 			elem = unit
 		}
-		go engine.Engine{}.Execute(elem, graph, shared2.EXECUTE_FOREVER)
+		go engine.Engine{}.Execute(elem, graph, shared.EXECUTE_FOREVER)
 	}
 
 	for i := range e.MADLX.Connectors {
-		go engine.Engine{}.Execute(e.MADLX.Connectors[i].Type, e.MADLX.Connectors[i].Graph, shared2.EXECUTE_FOREVER)
+		go engine.Engine{}.Execute(e.MADLX.Connectors[i].Type, e.MADLX.Connectors[i].Graph, shared.EXECUTE_FOREVER)
 	}
 }
 
-func (ee *EE) DeployApp(mee madl2.MADL, mapp madl2.MADL) {
+func (ee *EE) DeployApp(mee madl.MADL, mapp madl.MADL) {
 
-	elems := []madl2.Element{}
+	elems := []madl.Element{}
 	for i := range mapp.Components {
 		elems = append(elems, mapp.Components[i])
 	}
@@ -63,7 +63,7 @@ func (ee *EE) DeployApp(mee madl2.MADL, mapp madl2.MADL) {
 	ee.MADLX = mee
 }
 
-func (ee *EE) Deploy(m madl2.MADL) {
+func (ee *EE) Deploy(m madl.MADL) {
 
 	ee.MADLX = m
 }
