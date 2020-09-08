797f0d9329cfa7c89112abe7143a19115d71a201
Daniel <dcb@cin.ufpe.br>
2020-09-08 04:12:56 -0300

    Add Unmarshal to HttpResponse + fix blank first line on body

--- src/gmidarch/development/messages/httpmessage.go
+++ src/gmidarch/development/messages/httpmessage.go
@@ -1,6 +1,7 @@
 package messages
 
 import (
+	"fmt"
 	"strings"
 )
 
@@ -20,6 +21,7 @@ type HttpRequest struct {
 func (req *HttpRequest) Unmarshal(payload []byte) {
 	req.Header.Fields = make(map[string]string)
 	lines := strings.Split(string(payload), "\n")
+	//fmt.Println("HttpResponse.Unmarshal lines:", lines)
 	bodyStarted := false
 	for _, line := range lines {
 		if req.Method == "" {
@@ -38,6 +40,7 @@ func (req *HttpRequest) Unmarshal(payload []byte) {
 
 		if strings.TrimSpace(line) == "" {
 			bodyStarted = true
+			continue
 		}
 
 		if !bodyStarted {
@@ -64,4 +67,34 @@ func (resp HttpResponse) Marshal() []byte {
 	response += "\n" + resp.Body
 
 	return []byte(response)
+}
+
+func (resp *HttpResponse) Unmarshal(payload string) {
+	//fmt.Println("HttpResponse.Unmarshal payload:", payload)
+	resp.Header.Fields = make(map[string]string)
+	lines := strings.Split(payload, "\n")
+	bodyStarted := false
+	for _, line := range lines {
+		if resp.Protocol == "" {
+			startLine := strings.Fields(line)
+			fmt.Println("HttpResponse.Unmarshal startLine:", startLine)
+			resp.Protocol = startLine[0]
+			resp.Status = startLine[1]
+
+			continue
+		}
+
+		if strings.TrimSpace(line) == "" {
+			bodyStarted = true
+			continue
+		}
+
+		if !bodyStarted {
+			header := strings.Split(line, ": ")
+			//fmt.Println("HttpResponse.Unmarshal header:", header)
+			resp.Header.Fields[header[0]] = header[1]
+		}else{
+			resp.Body += line
+		}
+	}
 }
