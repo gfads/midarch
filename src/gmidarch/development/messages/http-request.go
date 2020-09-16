2b3eea17be3ebb245c1cf75c2700fa012e63aa24
Daniel <dcb@cin.ufpe.br>
2020-09-16 16:38:54 -0300

    Breaking httpmessage into request and response

---
+++ src/gmidarch/development/messages/http-request.go
@@ -0,0 +1,62 @@
+package messages
+
+import (
+	"strings"
+)
+
+type Header struct {
+	Fields map[string]string
+}
+
+type HttpRequest struct {
+	Method string
+	Route string
+	QueryParameters string
+	Protocol string
+	Header Header
+	Body string
+}
+
+func (req *HttpRequest) Marshal() []byte {
+	request := req.Method + " " + req.Route + " " + req.Protocol + "\n"
+	for key, value := range req.Header.Fields {
+		request += key + ": " + value + "\n"
+	}
+	request += "\n" + req.Body
+
+	return []byte(request)
+}
+
+func (req *HttpRequest) Unmarshal(payload []byte) {
+	req.Header.Fields = make(map[string]string)
+	lines := strings.Split(string(payload), "\n")
+	//fmt.Println("HttpResponse.Unmarshal lines:", lines)
+	bodyStarted := false
+	for _, line := range lines {
+		if req.Method == "" {
+			startLine := strings.Fields(line)
+			req.Method = startLine[0]
+			req.Protocol = startLine[2]
+
+			route := strings.Split(startLine[1], "?")
+			req.Route = route[0]
+			if len(route) > 1 {
+				req.QueryParameters = route[1]
+			}
+
+			continue
+		}
+
+		if strings.TrimSpace(line) == "" {
+			bodyStarted = true
+			continue
+		}
+
+		if !bodyStarted {
+			header := strings.Split(line, ": ")
+			req.Header.Fields[header[0]] = header[1]
+		}else{
+			req.Body += line
+		}
+	}
+}
--- src/gmidarch/development/messages/http-response.go
+++ src/gmidarch/development/messages/http-response.go
@@ -1,56 +1,8 @@
 package messages
 
 import (
-	"fmt"
 	"strings"
 )
-
-type Header struct {
-	Fields map[string]string
-}
-
-type HttpRequest struct {
-	Method string
-	Route string
-	QueryParameters string
-	Protocol string
-	Header Header
-	Body string
-}
-
-func (req *HttpRequest) Unmarshal(payload []byte) {
-	req.Header.Fields = make(map[string]string)
-	lines := strings.Split(string(payload), "\n")
-	//fmt.Println("HttpResponse.Unmarshal lines:", lines)
-	bodyStarted := false
-	for _, line := range lines {
-		if req.Method == "" {
-			startLine := strings.Fields(line)
-			req.Method = startLine[0]
-			req.Protocol = startLine[2]
-
-			route := strings.Split(startLine[1], "?")
-			req.Route = route[0]
-			if len(route) > 1 {
-				req.QueryParameters = route[1]
-			}
-
-			continue
-		}
-
-		if strings.TrimSpace(line) == "" {
-			bodyStarted = true
-			continue
-		}
-
-		if !bodyStarted {
-			header := strings.Split(line, ": ")
-			req.Header.Fields[header[0]] = header[1]
-		}else{
-			req.Body += line
-		}
-	}
-}
 
 type HttpResponse struct {
 	Protocol string
@@ -77,7 +29,7 @@ func (resp *HttpResponse) Unmarshal(payload string) {
 	for _, line := range lines {
 		if resp.Protocol == "" {
 			startLine := strings.Fields(line)
-			fmt.Println("HttpResponse.Unmarshal startLine:", startLine)
+			//fmt.Println("HttpResponse.Unmarshal startLine:", startLine)
 			resp.Protocol = startLine[0]
 			resp.Status = startLine[1]
 
