0a5c7fe6614f8672a282b94f7bbf72b7911bbace
Daniel Brandão <dcb@cin.ufpe.br>
2021-08-10 10:16:14 -0300

    MAPEK components from previous version

---
+++ src/gmidarch/development/components/adaptive/executor.go
@@ -0,0 +1,46 @@
+package components
+
+import (
+	"gmidarch/development/artefacts/graphs"
+	"gmidarch/development/messages"
+	"shared"
+)
+
+type Executor struct {
+	Behaviour string
+	Graph     graphs.ExecGraph
+}
+
+func NewExecutor() Executor {
+
+	// create a new instance of client
+	r := new(Executor)
+	r.Behaviour = "B = InvP.e1 -> I_Process -> InvR.e2 -> B"
+
+	return *r
+}
+
+func (e Executor) Selector(elem interface{}, elemInfo [] *interface{}, op string, msg *messages.SAMessage, info []*interface{}, r *bool) {
+	e.I_Process(msg, info)
+}
+
+func (Executor) I_Process(msg *messages.SAMessage, info [] *interface{}) {
+	plan := msg.Payload.(shared.AdaptationPlan)
+	unitCommand := shared.UnitCommand{}
+
+	if len(plan.Operations) > 0 { // TODO
+		pluginName := plan.Params[plan.Operations[0]][0]
+		plg := shared.LoadPlugin(pluginName)
+		getType, _ := plg.Lookup("Gettype")
+		elemType := getType.(func() interface{})()
+		getSelector, _ := plg.Lookup("Getselector")
+		funcSelector := getSelector.(func() func(interface{}, [] *interface{}, string, *messages.SAMessage, []*interface{}, *bool))()
+
+		unitCommand.Cmd = shared.REPLACE_COMPONENT
+		unitCommand.Params = plg
+		unitCommand.Type = elemType
+		unitCommand.Selector = funcSelector
+	}
+	*msg = messages.SAMessage{Payload: unitCommand}
+	//fmt.Printf("Executor:: %v\n",msg.Payload)
+}
