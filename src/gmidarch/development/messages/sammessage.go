27f71e72ffcdaec6da8b7f4805d342c5dd140f06
Daniel Brandão <dcb@cin.ufpe.br>
2022-05-11 23:04:42 -0300

    The message now saves the client address

--- src/gmidarch/development/messages/sammessage.go
+++ src/gmidarch/development/messages/sammessage.go
@@ -2,7 +2,8 @@ package messages
 
 // Software architecture message
 type SAMessage struct {
-	From string   `json:"From"`
-	To string     `json:"To"`
+	From string   		`json:"From"`
+	To string     		`json:"To"`
+	ToAddr string		`json:"ToAddr"`
 	Payload interface{} `json:"Payload"`
 }
