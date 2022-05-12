6f1722dec98f81c2bcdaa367e1d92b53e9d9e317
Daniel Brandão <dcb@cin.ufpe.br>
2022-05-11 23:06:03 -0300

    Adding connection pool to SRH

--- src/gmidarch/development/messages/srhinfo.go
+++ src/gmidarch/development/messages/srhinfo.go
@@ -1,18 +1,34 @@
 package messages
 
 import (
-	"net"
+"net"
 )
 
 type SRHInfo struct {
-	EndPoint      EndPoint               // host, port
-	Ln            net.Listener           // Listener
-	Conns         []net.Conn             // Set of connections
-	CurrentConn   net.Conn               // Current connection
-	RcvedMessages chan ReceivedMessages  // Buffer of messages received by the server
+	EndPoint      	EndPoint               	// host, port
+	Ln            	net.Listener           	// Listener
+	Conns         	[]net.Conn             	// Set of connections
+	CurrentConn		net.Conn               	// Current connection
+	RcvedMessages 	chan ReceivedMessages	// Buffer of messages received by the server
+	Clients			[]*Client				// Connection Pool, possible connected
+	Counter			int
 }
 
 type ReceivedMessages struct {
 	Chn net.Conn
 	Msg []byte
+}
+
+type Client struct {
+	Ip	string
+	Connection	net.Conn
+}
+
+func (i SRHInfo) GetClientFromAddr(addr string, clients []*Client) *Client {
+	for _, client := range clients {
+		if client.Ip == addr {
+			return client
+		}
+	}
+	return nil
 }
